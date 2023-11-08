eda94c7812abdb93cb2ad19ab1fd258cc21eb77d
Simon Myrvig <simo008s@mail.hansenberg.dk>
2023-11-08 20:50:35 +0100

    Initial setup

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Wizard
+{
+    class Program
+    {
+        public void Run()
+        {
+            // Your program logic here
+        }
+
+        public void Main(string[] args)
+        {
+            Program program = new Program();
+            program.Run();
+            Console.ReadKey();
+        }
+    }
+}
