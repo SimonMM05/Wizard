57788f4fd921f62bcb17b07587d3f641eff4d6d5
Simon Myrvig <simo008s@mail.hansenberg.dk>
2023-11-08 21:53:24 +0100

    Nearly done, need to add which spell the wizard has casted.

--- Program.cs
+++ Program.cs
@@ -1,18 +1,95 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace Wizard
+namespace Wizard_Program
 {
-    class Program
+    class Wizard
     {
-        public void Run()
+        public string name;
+        public string Spell1;
+        public string Spell2;
+        public int spellSlots;
+        public float experience;
+        public List<string> castedSpells;
+
+        public Wizard(string name, string spell1, string spell2, int spellSlots, float experience)
         {
-            // Your program logic here
+            this.name = name;
+            Spell1 = spell1;
+            Spell2 = spell2;
+            this.spellSlots = spellSlots;
+            this.experience = experience;
+            castedSpells = new List<string>();
         }
 
-        public void Main(string[] args)
+        public void CastSpell()
         {
-            Program program = new Program();
-            program.Run();
+            Random random = new Random();
+
+            if (spellSlots >= 1)
+            {
+                int randomNumber = random.Next(1, 3);
+                string spellToCast = (randomNumber == 1) ? Spell1 : Spell2;
+                castedSpells.Add(spellToCast);
+
+                Console.WriteLine(name + " has casted " + spellToCast);
+                spellSlots--;
+
+                if (randomNumber == 1)
+                {
+                    experience += 0.5f;
+                }
+                else
+                {
+                    experience += 1f;
+                }
+
+                if (experience >= 1)
+                {
+                    BuySpellSlots();
+                }
+            }
+            else
+            {
+                Console.WriteLine(name + " has no spell slots left.");
+            }
+        }
+
+        public void BuySpellSlots()
+        {
+            spellSlots += (int)experience;
+            experience -= (int)experience;
+        }
+
+        public void PrintCastedSpells()
+        {
+            Console.WriteLine(name + " has casted the following spells:");
+            foreach (string spell in castedSpells)
+            {
+                Console.WriteLine(spell);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Wizard wizard01 = new Wizard("Mlon Eusk", "Money strike", "Tesla bolt", 3, 0f);
+
+            Console.WriteLine("The wizard " + wizard01.name + " has entered the world");
+            Console.WriteLine();
+
+            while (wizard01.spellSlots > 0 && wizard01.experience >= 1)
+            {
+                wizard01.CastSpell();
+            }
+
+            Console.WriteLine(wizard01.name + " has Experience: " + wizard01.experience);
+            Console.WriteLine(wizard01.name + " has Spell Slots: " + wizard01.spellSlots);
+
+            wizard01.PrintCastedSpells();
+
             Console.ReadKey();
         }
     }
